5e651f2a9639fcc811f887507b205a156e28be88
Jb Maison <jb.vaugi@hotmail.fr>
2026-01-05 17:17:09 +0100

    obj Annonces / get list et byID

---
+++ models/Annonces.class.php
@@ -0,0 +1,23 @@
+<?php
+
+    require_once 'modele/ModeleAnnoces.class.php';
+    require_once 'modele/ModeleAnnocesException.class.php';
+
+
+    class Annonces extends ModeleAnnonces {
+        
+        public function getAnnoncesById() {
+            $sql = 'SELECT * FROM annonces ORDER BY id DESC';
+            $annonces = $this->executerRequete($sql);
+            return $annonces;
+        }
+
+        public function getAnnonce($idAnnonce) {
+            $sql = 'SELECT * FROM annonces WHERE id = ?';
+            $annonce = $this->executerRequete($sql, array($idAnnonce));
+            if ($annonce->rowCount() == 1)
+                return $annonce->fetch();  // Accès à la première ligne de résultat
+            else
+                throw new ModeleAnnoncesException("Aucune annonce ne correspond à l'identifiant '$idAnnonce'");
+        }
+    }
