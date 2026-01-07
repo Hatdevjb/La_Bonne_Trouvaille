9b5880d15538a9a05cd512a1d96d06534f665dc0
Jb Maison <jb.vaugi@hotmail.fr>
2026-01-07 22:52:45 +0100

    style change/ gabarit change

--- vues/gabarie.php
+++ vues/gabarie.php
@@ -2,21 +2,75 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8" />
-        <link rel="stylesheet" href="contenu/style.css" />
-        <title><?= $titre ?></title>
+         <title><?= $titre ?></title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="icon" type="images/png" href="./images/logoLBT.png">
+        <link rel="stylesheet" href="contenue/style.css" />
+        <script src="https://kit.fontawesome.com/6c5bf009e7.js" crossorigin="anonymous"></script>
+
     </head>
     <body>
-        <div id="global">
-            <header>
-                <a href="index.php"><h1 id="titreBlog">Mon test </h1></a>
-                <p>Je vous souhaite la bienvenue sur cette affichge  test</p>
-            </header>
-            <div id="contenu">
-                <?= $contenu ?>
-            </div> <!-- #contenu -->
-            <footer id="piedBlog">
-                Blog réalisé avec PHP, HTML5 et CSS.
-            </footer>
-        </div> <!-- #global -->
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">
+                <img src="images/LBT_Big_Logo.png" alt="Logo Big LBT" width="300" height="200">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse flex-grow-0" id="navbarNav">
+                <ul class="navbar-nav text-center">
+                    <li class="nav-item"><a class="nav-link" href="index.html">Accueil</a></li>
+                
+                </ul> 
+            </div>
+            <!--  user connection / annonces général / profil / annonce  -->
+            <div class="user-bar" style="position: relative;">
+                <div class="user-section">
+                    <a href="#" class="user-link">
+                        <i class="fa-regular fa-user"></i>
+                        <span>Me connecter</span>
+                    </a>
+                </div>
+
+                <div class="favorites-section">
+                    <a href="#" class="favorites-link">
+                        <i class="fa-solid fa-clipboard-list"></i>
+                        <span>Mes Annonces</span>
+                    </a>
+                </div>
+    
+            </div>
+        </div>
+    </nav>
+    <!-- FIN NAVBAR -->
+
+    <!-- MAIN -->
+        <div id="contenu">
+            <?= $contenu ?>
+        </div>
+    <!-- #contenu -->
+      
+
+
+    <!-- FOOTER -->
+    <footer class="footer">
+        <a class="footer-logo" href="index.html">
+            <img src="img/LBT_Big_Logo.png" alt="Logo Big LBT" width="250" height="150">
+        </a>
+        <p class="Mention">Mention Légal</p>
+        <p class="reseaux">Nos reseaux: 
+            <a href="https://github.com/Hatdevjb/La_Bonne_Trouvaille"><i class="fa-brands fa-github"></i></a>
+        </p>
+        <div class="title-line"></div>
+        <p class="copyright">© Copyrights 2025, All Rights Reserved by LBT.Inc</p>
+    </footer>
+    <!-- FIN FOOTER -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
--- vues/vueAnnonce.php
+++ vues/vueAnnonce.php
@@ -1,5 +1,5 @@
 <?php $this->titre = "Mon TestClient - " . $annonce['titre']; ?>
-
+<!-- 
 <article>
     <header>
         <h1 class="titreAnnonce"><?= $annonce['titre'] ?></h1>
@@ -10,9 +10,5 @@
 <form method="post" action="index.php?action=commenter">
     <input id="auteur" name="auteur" type="text" placeholder="Votre pseudo" 
            required /><br />
-    <textarea id="txtCommentaire" name="contenu" rows="4" 
-              placeholder="Votre commentaire" required></textarea><br />
-    <input type="hidden" name="id" value="<?= $annonce['id'] ?>" />
-    <input type="submit" value="Commenter" />
-</form>
+</form> -->
 
