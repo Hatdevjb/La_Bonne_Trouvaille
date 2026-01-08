6cdacc5577d9bbeb32ea9c34505153a21576fcb8
Jb Maison <jb.vaugi@hotmail.fr>
2026-01-08 13:07:25 +0100

    test ctrlImg

--- Controleur/ControleurAccueilAnn.php
+++ Controleur/ControleurAccueilAnn.php
@@ -14,6 +14,12 @@
     // Affiche la liste de tous les annonces du blog
         public function accueilAnn() {
             $annonces = $this->annonce->getlistAnnonces();
+            
+            // Ajoute l'image à chaque annonce
+            foreach ($annonces as &$annonce) {
+                $annonce['image'] = $this->annonce->getImageAnnonce($annonce['id_annonce']);
+            }
+            
             $vue = new Vue("AccueilAnnonce");
             $vue->generer(array('annonces' => $annonces));
         }
